aaec15ef0674d70a13be963594afe7836a08b17b
Ross Chapman <rosschapman@users.noreply.github.com>
2020-05-16 14:29:52 -0700

    Update types.ts

--- types.ts
+++ types.ts
@@ -2,5 +2,5 @@
 type NonBodyParameter = Extract<Parameter, BodyParameter>;
 
 // React
-type ChildrenProps<Props> = Parameters<Props["children"]>[0];
-type ChildrenProp<Props, Prop> = ChildrenProps<Props>[Prop];
+type ChildrenProps<Class> = Parameters<Class["props"]["children"]>[0];
+type ChildrenProp<Class, Prop> = ChildrenProps<Class>[Prop];
